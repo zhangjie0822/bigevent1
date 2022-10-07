c5a34b66cc17c2f8ccc0f94ea9a48eb06121edc3
zhangjie <15295492931@163.com>
2022-10-07 09:47:55 +0800

    个人中心功能完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,71 @@
-$(function(){
+$(function () {
     getUserInfo()
     var layer = layui.layer
-    $('#btnLogout').on('click',function(){
-        layer.confirm('确认退出?', {icon: 3, title:'提示'}, function(index){
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确认退出?', { icon: 3, title: '提示' }, function (index) {
             //do something
             localStorage.removeItem('token')
             location.href = './login.html'
             layer.close(index);
-          });
+        });
     })
 })
-function getUserInfo(){
+function getUserInfo() {
     $.ajax({
-        method:'get',
-        url:'/my/userinfo',
+        method: 'get',
+        url: '/my/userinfo',
         // headers:{
         //     Authorization:localStorage.getItem('token')||''
         // },
-        success(res){
+        success(res) {
             // console.log(res);
-            if(res.status !== 0){
+            if (res.status !== 0) {
                 return layui.layer.msg('获取用户信息失败！')
             }
             renderAvatar(res.data)
         },
         // complete:function(res){
-            
+
         //     if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
         //         localStorage.removeItem('token')
         //         location.href = '/login.html'
         //     }
         // }
-        
+
     })
 }
-function renderAvatar(user){
-    const name = user.nickname || user.username
-    // console.log(name)
-    $('#welcome').html('欢迎&nbsp;&nbsp;'+name)
-    if(user.user_pic !== null){
-        $('.layui-nav-img').attr('src',user.user_pic).show()
+// function renderAvatar(user){
+//     const name = user.nickname || user.username
+//     console.log(name)
+//     $('#welcome').html('欢迎&nbsp;&nbsp;'+name)
+//     if(res.data.user_pic){
+//         $('.layui-nav-img').attr('src',res.data.user_pic).show()
+//         $('.text-avatar').hide()
+//     }else{
+//         $('.layui-nav-img').hide()
+//         var first = name[0].toUpperCase()
+//         $('.text-avatar').html(first).show()
+//     }
+// }
+// 渲染用户的头像
+function renderAvatar(user) {
+    // 1. 获取用户的名称
+    var name = user.nickname || user.username
+    // 2. 设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 3. 按需渲染用户的头像
+    if (user.user_pic !== null) {
+        // 3.1 渲染图片头像
+        $('.layui-nav-img')
+            .attr('src', user.user_pic)
+            .show()
         $('.text-avatar').hide()
-    }else{
+    } else {
+        // 3.2 渲染文本头像
         $('.layui-nav-img').hide()
         var first = name[0].toUpperCase()
-        $('.text-avatar').html(first).show()
+        $('.text-avatar')
+            .html(first)
+            .show()
     }
 }
--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -27,4 +27,53 @@ $(function () {
             .attr('src', blobUrl)  // 重新设置图片路径
             .cropper(options)        // 重新初始化裁剪区域
     })
+    // $('#btnConfirm').on('click',function(){
+    //     var dataURL = $image
+    //   .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+    //     width: 100,
+    //     height: 100
+    //   })
+    //   .toDataURL('image/png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+    //   $.ajax({
+    //     method:'post',
+    //     url:'/my/update/avatar',
+    //     data:{
+    //         avatar:dataURL
+    //     },
+    //     success(res){
+    //         if(res.code !== 0) return layer.msg('上传头像失败')
+    //         layer.msg('上传头像成功')
+    //         window.parent.getUserInfo()
+    //     },
+    //     error(err){
+    //         console.log(err)
+    //     }
+    //   })
+    // })
+    // 为确定按钮，绑定点击事件
+  $('#btnConfirm').on('click', function() {
+    // 1. 要拿到用户裁剪之后的头像
+    var dataURL = $image
+      .cropper('getCroppedCanvas', {
+        // 创建一个 Canvas 画布
+        width: 100,
+        height: 100
+      })
+      .toDataURL('image/png') // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+    // 2. 调用接口，把头像上传到服务器
+    $.ajax({
+      method: 'POST',
+      url: '/my/update/avatar',
+      data: {
+        avatar: dataURL
+      },
+      success: function(res) {
+        if (res.status !== 0) {
+          return layer.msg('更换头像失败！')
+        }
+        layer.msg('更换头像成功！')
+        window.parent.getUserInfo()
+      }
+    })
+  })
 })
